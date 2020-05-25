d9b8baa7647991360104e4059e3b82bee8a81b0f
7Kushal <46702993+7Kushal@users.noreply.github.com>
2020-05-25 15:08:40 +0530

    Add files via upload
    Two number sum finding using 2 pointer algorithm in O(n) time complexity

---
+++ Two_pointer_algo.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Two_pointer_algo {
+    public static void main(String[] args){
+        System.out.print("Enter the length of array = ");
+        Scanner in = new Scanner(System.in);
+        int l = in.nextInt();
+        System.out.print("Enter the array = ");
+        int[] a = new int[l];
+
+        for(int k =0 ; k <l ; k++){
+            a[k]=in.nextInt();
+        }
+        System.out.print("Enter the sum element you want to find = ");
+        int sum = in.nextInt();
+        System.out.println(isEqualTo(a,l,sum));
+    }
+
+    public static boolean isEqualTo(int[] arr , int len , int n){
+        Arrays.sort(arr);
+        int j = len -1;
+        int i = 0 ;
+        while(i<j){
+            if((arr[i]+arr[j])<n)
+                i++;
+            else if((arr[i]+arr[j])>n)
+                j--;
+            else
+                return true;
+        }
+        return false;
+    }
+}
